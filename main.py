9576d70d874428d29328f8121860c57b0e39f81c
kolgin216 <kolbik@bk.ru>
2022-02-10 14:02:40 +0300

    Find max square

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
-sides = sorted(sides, reverse=True)
-smax = 0
-for i in range(len(sides)):
-	for j in range(i + 1, len(sides)):
-		for k in range(j + 1, len(sides)):
-			a = sides[i]
-			b = sides[j]
-			c = sides[k]
-			if a + b > c and a + c > b and b + c > a:
-				p = (a + b + c) / 2
-				s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
-				if s > smax:
-					smax = s
-					print("Максимальная площадь треугольника", smax)
+import cmath
+
+a=float(input("Число a "))
+b=float(input("Число b "))
+c=float(input("Число c "))
+
+print("Наше уравнение имеет вид: ",f"{a}*x^2+{b}*x+{c}")
+disc = (b**2) - (4*a*c)
+if disc>0 :
+	x1 = (-b-cmath.sqrt(disc))/(2*a)
+	x2 = (-b+cmath.sqrt(disc))/(2*a)
+	print("x1=",x1,"\nx2=",x2)
+elif disc == 0 :
+	x1= -b/(2*a)
+	print("Ответ x=",x1)
+else:
+	print("Корней нет")
